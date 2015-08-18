f75189c8f158d1dd64f33c0dcfe59a95f9b3f6a0
Connie Leung <cleung334@gmail.com>
2015-08-19 00:26:12 +0800

    add routing to bower_components folder
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- public/js/app.js
+++ public/js/app.js
@@ -112,7 +112,8 @@ app.controller('EmployeesCtrl', ['$scope', 'EmployeeService', function($scope, s
   }, _handleError);
 }]);
 
-app.controller('EmployeeCtrl', ['$scope', '$routeParams', 'EmployeeService', 'TeamService', '$q', 'config', '$route',
+app.controller('EmployeeCtrl', ['$scope', '$routeParams', 
+  'EmployeeService', 'TeamService', '$q', 'config', '$route',
   function($scope, $routeParams, employee, team, $q, config, $route) {
 
     $scope.address = {};
